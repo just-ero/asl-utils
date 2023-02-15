7938bf4540253fc9b8a80433a0fe0c723d8c85f5
Ero <imjustero@gmail.com>
2023-02-15 20:16:06 +0100

    Add final ReadKey

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using AslUtils;
 using Spectre.Console;
 
@@ -97,3 +98,6 @@ if (choices.Contains(CascadiaCode))
         AnsiConsole.WriteLine();
     }
 }
+
+AnsiConsole.Markup("[Green]asl-help execution finished![/] Press any key to exit... ");
+Console.ReadKey(true);
